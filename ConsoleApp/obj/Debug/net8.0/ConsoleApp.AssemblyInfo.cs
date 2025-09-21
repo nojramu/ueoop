03650418c6a7c47eb9560b9752e1d29c8746dd9c
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-09-21 03:23:05 +0000

    rebuild 2

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab5e438cb88be35cd64210e00860e9b980b4a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bffcece02486136379aa57035a1b9c00e53e7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
