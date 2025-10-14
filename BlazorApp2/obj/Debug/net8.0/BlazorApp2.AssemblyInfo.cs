356dae8e3ce57e99ba76efdf9b3560767529c20e
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-10-14 22:03:35 +0000

    making a login

--- BlazorApp2/obj/Debug/net8.0/BlazorApp2.AssemblyInfo.cs
+++ BlazorApp2/obj/Debug/net8.0/BlazorApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f95c575c8e6acc9bcc4c95e13aacb4800b283ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03650418c6a7c47eb9560b9752e1d29c8746dd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1bffcece02486136379aa57035a1b9c00e53e7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03650418c6a7c47eb9560b9752e1d29c8746dd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
