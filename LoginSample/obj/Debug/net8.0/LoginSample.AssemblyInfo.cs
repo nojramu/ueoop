00be4f471c1d39a8a31696cd34eb23d6a16f4c5d
marjon umbay <97776511+nojramu@users.noreply.github.com>
2025-10-15 00:57:23 +0000

    debug

--- LoginSample/obj/Debug/net8.0/LoginSample.AssemblyInfo.cs
+++ LoginSample/obj/Debug/net8.0/LoginSample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginSample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92a1ce558c87ba5656133104bc92d37e28b4790e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b427c2ec024a200632cd8e52a7bf82a56045e9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginSample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginSample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
